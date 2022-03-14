8c22ea71eb9733e514abbec9ab79e7d04f837834
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-03-14 11:39:16 -0400

    Added and completed controllers

---
+++ controllers/dataController.js
@@ -0,0 +1,61 @@
+const Product = require('../models/products')
+
+const dataController = {
+    index(req, res, next){
+        Product.find({}, (err, allProducts) => {
+            if(err) {
+                res.status(404).send({ msg: err.message })
+            }
+            else {
+                res.locals.data.products = allProducts
+                next()
+            }
+        })
+    },
+    create(req, res, next){
+        Product.create(req.body, (err, createdProduct) => {
+            if(err) {
+                res.status(404).send({ msg: err.message })
+            }
+            else {
+                res.locals.data.product = createdProduct
+                next()
+            }
+        })
+    },
+    show(req, res, next){
+        Product.findById(req.params.id, (err, foundProduct) => {
+            if(err) {
+                res.status(404).send({ msg: err.message })
+            }
+            else {
+                res.locals.data.product = foundProduct
+                next()
+            }
+        })
+    },
+    update(req, res, next){
+        Product.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedProduct) => {
+            if(err) {
+                res.status(404).send({ msg: err.message})
+            }
+            else {
+                res.locals.data.product = updatedProduct
+                next()
+            }
+        })
+    },
+    destroy(req, res, next){
+        Product.findByIdAndRemove(req.params.id, (err, deletedProduct) => {
+            if(err) {
+                res.status(404).send({ msg: err.message })
+            }
+            else {
+                res.locals.data.product = deletedProduct
+                next()
+            }
+        })
+    }
+}
+
+module.exports = dataController
---
+++ controllers/routeControllers.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const viewController = require('./viewController')
+const dataController = require('./dataController')
+
+// Index route
+router.get('/', dataController.index, viewController.index)
+// New route
+router.get('/new', viewController.new)
+// Delete
+router.delete('/:id', dataController.destroy, viewController.redirectHome)
+// Update
+router.put('/:id', dataController.update, viewController.redirectShow)
+// Create
+router.post('/', dataController.create, viewController.redirectHome)
+// Edit
+router.get('/:id/edit', dataController.show, viewController.edit)
+// Show
+router.get('/:id', dataController.show, viewController.show)
+
+module.exports = router
